6e933edd1a54a9b445274e333724b5918b0d5daa
Titus Denis <deetitos@gmail.com>
2023-11-07 15:26:36 -0800

    done

--- execve.c
+++ execve.c
@@ -1,1 +1,23 @@
-#
+/**
+ *child_father_pid - Differentiates child pid from parent pid
+ *
+ *Return: 0
+ */
+int child_father_pid(void)
+{
+	pid_t my_pid, child_pid;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error");
+		return (1);
+	}
+	my_pid = getpid();
+	printf("My pid is: %u\n", my_pid);
+	if (child_pid == 0)
+		printf("(%u) Nooooooo!\n", my_pid);
+	else
+		printf("(%u) %u, I am your father\n", my_pid, child_pid);
+	return (0);
+}
